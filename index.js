f19455f1f5b4881f407a9afb6aacb263d7489a99
Joel Reske <joelreske@gmail.com>
2017-12-04 14:19:17 -0500

    Some formatting changes - also a change to the permissions scope which...... helped?

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email playlist-read-private';
+  var scope = 'playlist-read-private user-read-private user-read-email';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
